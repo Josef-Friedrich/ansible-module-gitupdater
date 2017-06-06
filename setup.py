4911676757851cfb64d9c967202853381f5a1f43
Josef Friedrich <josef@friedrich.rocks>
2017-06-06 18:30:44 +0200

    Delete dependencies from setup.py
    The dependencies are now stored in the file requirements.txt

--- setup.py
+++ setup.py
@@ -7,22 +7,4 @@ setup(
     author_email='josef@friedrich.rocks',
     license='GPL3',
     url='https://github.com/Josef-Friedrich/ansible-module-shellmarks',
-    install_requires=[
-        'ansible',
-        'gitup',
-        # test/utils/tox/requirements.txt
-        'nose',
-        'mock >= 1.0.1, < 1.1',
-        'passlib',
-        'coverage',
-        'coveralls',
-        'unittest2',
-        'redis',
-        'python-memcached',
-        'python-systemd',
-        'pycrypto',
-        'botocore',
-        'boto3',
-        'pytest',
-    ]
 )
