c206721f8ad976e01779c90611e66fa26d3eabbd
Josef Friedrich <josef@friedrich.rocks>
2017-06-06 21:12:59 +0200

    Add new test

--- test/test_gitup.py
+++ test/test_gitup.py
@@ -3,10 +3,11 @@ from ansible.compat.tests import unittest
 # import unittest
 import mock
 import gitupdater
-
+import tempfile
 from gitup import config as gitup_config
 
 default_config_path = gitup_config.get_default_config_path()
+tmp_dir = tempfile.mkdtemp()
 
 class TestFunction(unittest.TestCase):
 
@@ -14,7 +15,7 @@ class TestFunction(unittest.TestCase):
     def test_mock(self, AnsibleModule):
         module = AnsibleModule.return_value
         module.params = {
-            'path': '/tmp',
+            'path': tmp_dir,
             'state': 'present',
         }
         module.check_mode = False
@@ -28,4 +29,11 @@ class TestFunction(unittest.TestCase):
         assert(mock.call(argument_spec=expected,
                supports_check_mode=True) == AnsibleModule.call_args)
 
-        self.assertTrue('/tmp' in open(default_config_path).read())
+        self.assertTrue(tmp_dir in open(default_config_path).read())
+
+        module.params = {
+            'path': tmp_dir,
+            'state': 'absent',
+        }
+        gitupdater.main()
+        self.assertFalse(tmp_dir in open(default_config_path).read())
